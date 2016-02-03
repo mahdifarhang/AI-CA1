eddf99c66c36a6e0526cccf7c1d9f70c9ddb93d7
Mahdi <mahdi@localhost.localdomain>
2016-02-04 01:14:16 +0330

    finishing DFS and BFS

--- code.py
+++ code.py
@@ -1,8 +1,6 @@
 from numpy import genfromtxt
-from itertools import combinations as com
 from time import time
 n_queens = 8
-input_file_name = 'in3.csv'
 
 def num_of_column_and_row_threats(my_data):
 	column_threats = 0
@@ -11,14 +9,14 @@ def num_of_column_and_row_threats(my_data):
 		num_of_queens_column = 0
 		num_of_queens_row = 0
 		for j in range(n_queens):
-			if (my_data[j][1] == i + 1):
-				num_of_queens_column += 1
 			if (my_data[j][0] == i + 1):
 				num_of_queens_row += 1
-		if (num_of_queens_column > 1):
-			column_threats += num_of_queens_column - 1
+			if (my_data[j][1] == i + 1):
+				num_of_queens_column += 1
 		if (num_of_queens_row > 1):
 			row_threats += num_of_queens_row - 1
+		if (num_of_queens_column > 1):
+			column_threats += num_of_queens_column - 1
 	return column_threats + row_threats
 
 # we could count number of rows or columns without queens and the answer would have been the same
@@ -47,7 +45,7 @@ def danger(my_data):
 		for j in range(i + 1, n_queens):
 			if(my_data[i][0] == my_data[j][0] or my_data[i][1] == my_data[j][1] or abs(my_data[i][0] - my_data[j][0]) == abs(my_data[i][1] - my_data[j][1])):
 				dangers += 1
-	return int(dangers / 2)
+	return int(dangers)
 
 
 
@@ -60,73 +58,86 @@ num_of_moves = 8
 
 def is_duplicate_element(my_data, element):
 	num_of_existed = 0
-	for i in range(n_queens):
-		if (my_data[i][0] == element[0] and my_data[i][1] == element[1]):
+	for i in my_data:
+		if (i == element):
 			if (num_of_existed > 0):
 				return True
 			num_of_existed += 1
 	return False
 
 def move_if_possible(my_data, queen_no, move_no):
-	temp = []
-	temp.append(my_data[queen_no][0])
-	temp.append(my_data[queen_no][1])
+	temp = [my_data[queen_no][0], my_data[queen_no][1]]
 	if (move_no == 1):
 		my_data[queen_no] = [my_data[queen_no][0] - 1, my_data[queen_no][1]]
 		if (my_data[queen_no][0] < 1 or is_duplicate_element(my_data, my_data[queen_no])):
-			my_data[queen_no][0] = temp[0]
-			my_data[queen_no][1] = temp[1]
+			my_data[queen_no] = temp
 			return False
 		return True
 	elif (move_no == 3):
 		my_data[queen_no] = [my_data[queen_no][0], my_data[queen_no][1] + 1]
 		if (my_data[queen_no][1] > num_of_moves or is_duplicate_element(my_data, my_data[queen_no])):
-			my_data[queen_no][0] = temp[0]
-			my_data[queen_no][1] = temp[1]
+			my_data[queen_no] = temp
 			return False
 		return True
 	elif (move_no == 5):
 		my_data[queen_no] = [my_data[queen_no][0] + 1, my_data[queen_no][1]]
 		if (my_data[queen_no][0] > num_of_moves or is_duplicate_element(my_data, my_data[queen_no])):
-			my_data[queen_no][0] = temp[0]
-			my_data[queen_no][1] = temp[1]
+			my_data[queen_no] = temp
 			return False
 		return True
 	elif (move_no == 7):
 		my_data[queen_no] = [my_data[queen_no][0], my_data[queen_no][1] - 1]
 		if (my_data[queen_no][1] < 1 or is_duplicate_element(my_data, my_data[queen_no])):
-			my_data[queen_no][0] = temp[0]
-			my_data[queen_no][1] = temp[1]
+			my_data[queen_no] = temp
 			return False
 		return True
 	elif (move_no == 0):
 		my_data[queen_no] = [my_data[queen_no][0] - 1, my_data[queen_no][1] - 1]
 		if (my_data[queen_no][0] < 1 or my_data[queen_no][1] < 1 or is_duplicate_element(my_data, my_data[queen_no])):
-			my_data[queen_no][0] = temp[0]
-			my_data[queen_no][1] = temp[1]
+			my_data[queen_no] = temp
 			return False
 		return True
 	elif (move_no == 2):
 		my_data[queen_no] = [my_data[queen_no][0] - 1, my_data[queen_no][1] + 1]
 		if (my_data[queen_no][0] < 1 or my_data[queen_no][1] > num_of_moves or is_duplicate_element(my_data, my_data[queen_no])):
-			my_data[queen_no][0] = temp[0]
-			my_data[queen_no][1] = temp[1]
+			my_data[queen_no] = temp
 			return False
 		return True
 	elif (move_no == 4):
 		my_data[queen_no] = [my_data[queen_no][0] + 1, my_data[queen_no][1] + 1]
 		if (my_data[queen_no][0] > num_of_moves or my_data[queen_no][1] > num_of_moves or is_duplicate_element(my_data, my_data[queen_no])):
-			my_data[queen_no][0] = temp[0]
-			my_data[queen_no][1] = temp[1]
+			my_data[queen_no] = temp
 			return False
 		return True
 	elif (move_no == 6):
 		my_data[queen_no] = [my_data[queen_no][0] + 1, my_data[queen_no][1] - 1]
 		if (my_data[queen_no][0] > num_of_moves or my_data[queen_no][1] < 1 or is_duplicate_element(my_data, my_data[queen_no])):
-			my_data[queen_no][0] = temp[0]
-			my_data[queen_no][1] = temp[1]
+			my_data[queen_no] = temp
 			return False
 		return True
+
+
+def move(my_data, queen_no, move_no):
+	if (move_no == 1):
+		my_data[queen_no] = [my_data[queen_no][0] - 1, my_data[queen_no][1]]
+	elif (move_no == 3):
+		my_data[queen_no] = [my_data[queen_no][0], my_data[queen_no][1] + 1]
+	elif (move_no == 5):
+		my_data[queen_no] = [my_data[queen_no][0] + 1, my_data[queen_no][1]]
+	elif (move_no == 7):
+		my_data[queen_no] = [my_data[queen_no][0], my_data[queen_no][1] - 1]
+	elif (move_no == 0):
+		my_data[queen_no] = [my_data[queen_no][0] - 1, my_data[queen_no][1] - 1]
+	elif (move_no == 2):
+		my_data[queen_no] = [my_data[queen_no][0] - 1, my_data[queen_no][1] + 1]
+	elif (move_no == 4):
+		my_data[queen_no] = [my_data[queen_no][0] + 1, my_data[queen_no][1] + 1]
+	elif (move_no == 6):
+		my_data[queen_no] = [my_data[queen_no][0] + 1, my_data[queen_no][1] - 1]
+
+
+
+
 
 def print_grid(my_data):
 	for i in range(n_queens):
@@ -152,41 +163,38 @@ def generate_next_children(my_data):
 	for i in range(n_queens):
 		for j in range(num_of_moves):
 			if(move_if_possible(my_data, i, j)):
-				# children.append([(q_pos if q_pos != parent_q else child_q) for q_pos in my_data])
 				children.append(copy_board(my_data))
-				if (j < 4):
-					move_if_possible(my_data, i, j + 4)
-				else:
-					move_if_possible(my_data, i, j - 4)
+				move(my_data, i, (j + 4) % 8)
 	return children
 
 
+all_moves = 0
 def IDS(root_grid):
 	i = 1
+	global all_moves
+	all_moves = 0
 	while (True):
 		result = DFS(root_grid, i)
 		if (result != None):
 			return result
 		i += 1
 
-
 def DFS(root_grid, limit):
-	s = [(root_grid, 0)]
-	all_moves = 0
-	while (len(s) > 0):
-		all_moves += 1
-		parent_grid, parent_level = s.pop(0)
-		if parent_level > limit:
-			continue
-		if (num_of_threats(parent_grid) == 0):
-			return parent_grid, limit, all_moves
-		for child_grid in generate_next_children(parent_grid):
-			s.append((child_grid, parent_level + 1))
-
-# def BFS()
-
-
-
+	if limit == 0:
+		if num_of_threats(root_grid) == 0:
+			return root_grid
+		else:
+			return None
+	for i in range(n_queens):
+		for j in range(num_of_moves):
+			if(move_if_possible(root_grid, i, j)):
+				global all_moves
+				all_moves += 1
+				temp = DFS(root_grid,limit - 1)
+				if temp is not None:
+					return temp
+				move(root_grid, i, (j + 4) % 8)
+	return None
 
 
 def A_star(root_grid):
@@ -208,20 +216,48 @@ def A_star(root_grid):
 			levels.append(level + 1)
 			measures.append(level + 1 + num_of_threats(temp[i]))
 
+def BFS(root_grid):
+	nodes = [root_grid]
+	v = {}
+	levels = [0]
+	moves = 0
+	while(True):
+		moves += 1
+		node = nodes.pop(0)
+		level = levels.pop(0)
+		if (str(node) not in v):
+			if (num_of_threats(node) == 0):
+				return node, level, moves
+			else:
+				v[str(node)] = 0
+			temp = generate_next_children(node)
+			for i in temp:
+				if (str(i) not in v):
+					nodes.append(i)
+					levels.append(level + 1)
 
 
 
 
+
+
+input_file_name = 'test_a.csv'
 
 temp_data = genfromtxt(input_file_name, delimiter=',')
 data = copy_board(temp_data)
+print(input_file_name)
+print_grid(data)
+print(num_of_threats(data))
+print()
 t1 = time()
-a, b, c = (A_star(data))
+a, b, c = (BFS(data))
+print_grid(a)
 t2 = time()
-if (a != None):
-	print_grid(a)
-else:
-	print(None)
-print(b, 'level')
-print(c, 'all moves')
-print(t2 - t1, 'time')
+# print('result')
+# if (a != None):
+# 	print_grid(a)
+# else:
+# 	print(None)
+# print('level = ' ,b)
+print('all moves =', all_moves)
+print('time = ', t2 - t1)
